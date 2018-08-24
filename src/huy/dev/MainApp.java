d39fecb974b20af50b3e24135e88491a61f65d32
unknown <huy.sirmaster@gmail.com>
2018-08-24 15:53:02 +0700

    person

---
+++ src/huy/dev/MainApp.java
@@ -0,0 +1,18 @@
+package huy.dev;
+
+import huy.dev.models.*;
+public class MainApp {
+		public static void main(String[]args) {
+			final String arrName[] = {"bao","Tu","Tap,","Teo","Name"};
+			final int arrOld[] = {21, 25, 26, 15, 16};
+			Person arrPerson[] = new Person[arrName.length];
+			for(int i=0; i < arrPerson.length; i++) {
+				Person p = new Person (arrName[i], arrOld[i]);
+				arrPerson[i]=p;
+			} //in ra
+			for(int i=0;i < arrPerson.length; i++) {
+				arrPerson[i].show();
+			}
+		}
+
+	}
